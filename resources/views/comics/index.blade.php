76b741ff102d6a7687250d2b0650db5c934ff523
RooRsmoka <chri.disa96@gmail.com>
2022-03-08 21:06:33 +0100

    changed view templates

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -5,21 +5,24 @@
     <thead>
         <tr>
             <th>Title</th>
+            <th>Description</th>
+            <th>Thumbnail</th>
             <th>Price</th>
             <th>Series</th>
-            <th>Description</th>
             <th>Sale Date</th>
+            <th>type</th>
         </tr>
     </thead>
     <tbody>
         @foreach($data as $comic)
             <tr>
-                <td>{{$comic->title}}</td>
-                <td>{{$comic->thumb}}</td>
-                <td>{{$comic->price}}</td>
-                <td>{{$comic->series}}</td>
-                <td>{{$comic->description}}</td>
-                <td>{{$comic->sale_date}}</td>
+                <td>{{ $comic->title }}</td>
+                <td>{{ $comic->description }}</td>
+                <td>{{ $comic->thumb }}</td>
+                <td>{{ $comic->price }}</td>
+                <td>{{ $comic->series }}</td>
+                <td>{{ $comic->sale_date }}</td>
+                <td>{{ $comic->type }}</td>
             </tr>
         @endforeach
     </tbody>
--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -1,26 +1,27 @@
 @extends('layouts.default')
 
 @section('content')
-<h3>Comics</h3>
-
 <ul>
     <li>
         <h2>Title: {{$comic->title}}</h2>
     </li>
     <li>
-        <h2>Price: {{$comic->price}}</h2>
-    </li>
-    <li>
-        <h2>Price: {{$comic->thumb}}</h2>
-    </li>
-    <li>
         <h2>Description: {{$comic->description}}</h2>
     </li>
     <li>
-       <h2>Sale Date: {{$comic->sale_date}}</h2>
+        <h2>Thumbnail: {{$comic->thumb}}</h2>
     </li>
     <li>
-        <h2>Series: {{$comic->series}}</h2>
+        <h2>Price: {{$comic->price}}</h2>
+    </li>
+    <li>
+       <h2>Series: {{$comic->series}}</h2>
+    </li>
+    <li>
+        <h2>Sale date: {{$comic->sale_date}}</h2>
+    </li>
+    <li>
+        <h2>Type: {{$comic->type}}</h2>
     </li>
 </ul>
 @endsection
