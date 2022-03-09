5f322244f7e8451dbaa92c60fa7dc7cb0f96b4d9
RooRsmoka <chri.disa96@gmail.com>
2022-03-09 19:58:15 +0100

    added link to index and fixed syntax

--- resources/views/comics/create.blade.php
+++ resources/views/comics/create.blade.php
@@ -1,38 +1,39 @@
 @extends('layouts.default')
 
 @section('content')
+<a href="{{ route('comics.index') }}">Back</a>
 <form action="{{ route('comics.store') }}" method="post">
 @csrf
     <div>
         <label>Title</label>
-        <input type="text" name="address">
+        <input type="text" name="title">
     </div>
     <div>
         <label>Description</label>
-        <input type="text" name="city">
-    </div>
-    <div>
-        <label>Thumbnail</label>
-        <input type="text" name="region">
-    </div>
-    <div>
-        <label>Price</label>
-        <input type="text" name="province">
-    </div>
-    <div>
-        <label>Series</label>
-        <input type="text" name="country">
-    </div>
-    <div>
-        <label>Sale date</label>
-        <input type="number" name="squareMeters">
-    </div>
-    <div>
-        <label>Type</label>
         <textarea name="description" cols="30" rows="10"></textarea>
     </div>
     <div>
-        <button type="reset">Indietro</button>
+        <label>Thumbnail</label>
+        <input type="text" name="thumb">
+    </div>
+    <div>
+        <label>Price</label>
+        <input type="text" name="price">
+    </div>
+    <div>
+        <label>Series</label>
+        <input type="text" name="series">
+    </div>
+    <div>
+        <label>Sale date</label>
+        <input type="date" name="sale_date">
+    </div>
+    <div>
+        <label>Type</label>
+        <input type="text" name="type">
+    </div>
+    <div>
+        <button type="reset">Reset</button>
         <button type="submit">Crea</button>
     </div>
 </form>
